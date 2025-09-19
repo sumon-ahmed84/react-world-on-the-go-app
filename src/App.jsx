c685a2687e3a22ad8308c14f004c2698f49b6539
sumon-ahmed84 <sumonahmed275060@gmail.com>
2025-09-19 23:22:29 +0600

    Fetch the all country api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
+import { Suspense } from 'react';
 import './App.css'
 import Countries from './components/countries/countries'
-
+const countriesPromise=fetch('https://openapi.programming-hero.com/api/all')
+.then(res=>res.json())
 
 function App() {
 
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<p>Counties loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
